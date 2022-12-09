8cb00386b6f1bb405631825f09ae48bb171132f6
chanpi23 <channycodez@gmail.com>
2022-12-08 22:55:05 -0500

    first commit ahhh

---
+++ app.js
@@ -0,0 +1,12 @@
+const squares= document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft= document.querySelector('#time-left')
+const score= document.querySelector('#score')
+
+let result =0
+
+function randomSquare  () {
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    });
+}
